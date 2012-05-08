278b073ef21941d722a9c5dc1ac050cd0ac74c07
Alex Tomkins <tomkins@darkzone.net>
2012-05-08 13:20:58 +0100

    Improve the Django admin interface

--- blanc_basic_podcast/podcast/admin.py
+++ blanc_basic_podcast/podcast/admin.py
@@ -3,4 +3,22 @@ from django.contrib.sites.models import Site
 from .models import PodcastFile
 
 
-admin.site.register(PodcastFile)
+class PodcastFileAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'date', 'file', 'description')
+        }),
+        ('Advanced options', {
+            'fields': ('slug', 'published')
+        }),
+    )
+    date_hierarchy = 'date'
+    list_display = ('title', 'date', 'published')
+    list_editable = ('published',)
+    list_filter = ('published', 'date')
+    prepopulated_fields = {
+       'slug': ('title',)
+    }
+
+
+admin.site.register(PodcastFile, PodcastFileAdmin)
