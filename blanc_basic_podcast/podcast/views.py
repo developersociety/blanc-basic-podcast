be0a9da80d46630d8958aa95838c5c7c67dda375
Alex Tomkins <tomkins@darkzone.net>
2012-05-08 13:16:34 +0100

    Fix the list view for podcast files, and allow custom per page number in settings

--- blanc_basic_podcast/podcast/views.py
+++ blanc_basic_podcast/podcast/views.py
@@ -1,9 +1,15 @@
 from django.views.generic import ListView, DateDetailView
+from django.utils import timezone
+from django.conf import settings
 from .models import PodcastFile
 
 
 class PodcastFileListView(ListView):
-    queryset = PodcastFile.objects.filter(published=True)
+    paginate_by = getattr(settings, 'PODCAST_PER_PAGE', 10)
+
+    def get_queryset(self):
+        return PodcastFile.objects.filter(published=True,
+                date__lte=timezone.now())
 
 
 class PodcastFileDetailView(DateDetailView):
