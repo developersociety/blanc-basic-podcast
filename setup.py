c5f69f1518f8248d396af174d2092cca2ef247a6
Alex Tomkins <tomkins@darkzone.net>
2015-12-03 15:19:05 +0000

    Improved setup.py
    - Updated URL, maintainer, classifiers, license
    - Use setuptools only

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from codecs import open
+
+from setuptools import find_packages, setup
+
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
 
 
 setup(
     name='blanc-basic-podcast',
     version='0.2',
     description='Blanc Basic Podcast for Django',
-    long_description=open('README.rst').read(),
-    url='http://www.blanctools.com/',
-    maintainer='Alex Tomkins',
-    maintainer_email='alex@hawkz.com',
+    long_description=readme,
+    url='https://github.com/blancltd/blanc-basic-podcast',
+    maintainer='Blanc Ltd',
+    maintainer_email='studio@blanc.ltd.uk',
     platforms=['any'],
     install_requires=[
         'hsaudiotag>=1.1.1',
     ],
-    packages=[
-        'blanc_basic_podcast',
-        'blanc_basic_podcast.podcast',
-    ],
+    packages=find_packages(),
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
     ],
-    license='BSD-2',
+    license='BSD',
 )
