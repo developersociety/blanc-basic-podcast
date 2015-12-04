3ef1bcd416263cae6a36c8a850bf1b986c725697
Alex Tomkins <tomkins@darkzone.net>
2015-12-04 14:45:01 +0000

    Use add_domain from syndication framework

--- blanc_basic_podcast/feeds.py
+++ blanc_basic_podcast/feeds.py
@@ -3,6 +3,7 @@ import mimetypes
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.contrib.syndication.views import add_domain
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 
@@ -30,13 +31,8 @@ class BasicPodcastFeed(PodcastFeed):
     @property
     def itunes_image(self):
         file_url = staticfiles_storage.url(settings.PODCAST_IMAGE)
-
-        # Must be a full URL
-        if not (file_url.startswith('http://') or file_url.startswith('https://')):
-            domain = Site.objects.get_current().domain
-            file_url = 'http://%s%s' % (domain, file_url)
-
-        return file_url
+        domain = Site.objects.get_current().domain
+        return add_domain(domain=domain, url=file_url)
 
     def items(self):
         feed_limit = getattr(settings, 'PODCAST_FEED_LIMIT', 10)
@@ -53,13 +49,8 @@ class BasicPodcastFeed(PodcastFeed):
 
     def item_enclosure_url(self, obj):
         file_url = obj.file.url
-
-        # Must be a full URL
-        if not (file_url.startswith('http://') or file_url.startswith('https://')):
-            domain = Site.objects.get_current().domain
-            file_url = 'http://%s%s' % (domain, file_url)
-
-        return file_url
+        domain = Site.objects.get_current().domain
+        return add_domain(domain=domain, url=file_url)
 
     def item_enclosure_mime_type(self, obj):
         return mimetypes.guess_type(obj.file.name)[0]
