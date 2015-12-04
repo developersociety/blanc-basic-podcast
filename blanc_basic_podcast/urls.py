de3d330701a9c5f330e0a33088a51385957f5f4e
Alex Tomkins <tomkins@darkzone.net>
2015-12-04 11:55:47 +0000

    Don't use patterns for urls

--- blanc_basic_podcast/urls.py
+++ blanc_basic_podcast/urls.py
@@ -1,29 +1,26 @@
-from django.conf.urls import patterns, url
 from django.conf import settings
-import feeds
-import views
+from django.conf.urls import url
+
+from . import feeds, views
 
 
-# RSS feed
-feed_patterns = patterns('',
+# RSS feed - always enabled
+urlpatterns = [
     url(r'^feed/$',
         feeds.BasicPodcastFeed(),
         name='feed'),
-)
+]
 
 # Pages
-page_patterns = patterns('',
+page_patterns = [
     url(r'^$',
         views.PodcastFileListView.as_view(),
         name='file-list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
         views.PodcastFileDetailView.as_view(),
         name='file-detail'),
-)
+]
 
-# Build the final URL patterns, setting PODCAST_PAGES to False in settings.py
-# will disable pages.
-urlpatterns = feed_patterns
-
+# Setting PODCAST_PAGES to False in settings.py will disable pages.
 if getattr(settings, 'PODCAST_PAGES', True):
     urlpatterns += page_patterns
